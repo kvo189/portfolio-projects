fc2bea91ad19112f6b63deb649165d41e2ec5582
Khang Vo <kvo189@gmail.com>
2020-04-23 14:40:43 -0500

    Updates

--- precache-manifest.c1b4ae40a27e39b1094edda1e2757643.js
+++ precache-manifest.c1b4ae40a27e39b1094edda1e2757643.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef95e6515d38b05cefbea948920e1d99",
+    "revision": "d967f1571a074547fdce251a2c550ea1",
     "url": "/portfolio-projects/index.html"
   },
   {
-    "revision": "7e9dbdae89816ac2d08b",
+    "revision": "96a10eac0fc96a305c07",
     "url": "/portfolio-projects/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "1417404f380aed2b3e7b",
-    "url": "/portfolio-projects/static/css/main.72c670dc.chunk.css"
+    "revision": "37814304963410dbf85b",
+    "url": "/portfolio-projects/static/css/main.ec5fe1ab.chunk.css"
   },
   {
-    "revision": "7e9dbdae89816ac2d08b",
-    "url": "/portfolio-projects/static/js/2.152beff9.chunk.js"
+    "revision": "96a10eac0fc96a305c07",
+    "url": "/portfolio-projects/static/js/2.b8b9d8e7.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/portfolio-projects/static/js/2.152beff9.chunk.js.LICENSE.txt"
+    "url": "/portfolio-projects/static/js/2.b8b9d8e7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1417404f380aed2b3e7b",
-    "url": "/portfolio-projects/static/js/main.58c43060.chunk.js"
+    "revision": "37814304963410dbf85b",
+    "url": "/portfolio-projects/static/js/main.c1710b90.chunk.js"
   },
   {
     "revision": "fb1be4bf9b2244acf576",
