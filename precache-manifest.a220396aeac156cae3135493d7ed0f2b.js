85744e50a44e8e61650652902469d59412818abd
Khang Vo <kvo189@gmail.com>
2020-07-06 15:41:29 -0500

    Updates

---
+++ precache-manifest.a220396aeac156cae3135493d7ed0f2b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafc22a79370ab13629c28a92735ba09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b09906d69dec1c77ac2",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5685be1e5e3ac47354aa",
+    "url": "/static/css/main.bd28d012.chunk.css"
+  },
+  {
+    "revision": "6b09906d69dec1c77ac2",
+    "url": "/static/js/2.89ef92b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.89ef92b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5685be1e5e3ac47354aa",
+    "url": "/static/js/main.cfb9c6f7.chunk.js"
+  },
+  {
+    "revision": "4b9ad4b76bb6c5c0d301",
+    "url": "/static/js/runtime-main.cd51f6bd.js"
+  },
+  {
+    "revision": "970066877e23f18be031b6f076e1d0ea",
+    "url": "/static/media/NotesApp.97006687.png"
+  },
+  {
+    "revision": "c912ea819d3434917c655c94120466bf",
+    "url": "/static/media/Project_Saigon_IMG.c912ea81.png"
+  },
+  {
+    "revision": "7f8e612c5bb7003ef27be5e4ab623f6c",
+    "url": "/static/media/TicTacToe.7f8e612c.png"
+  }
+]);
